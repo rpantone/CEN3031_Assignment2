898bb1497a09d5208cf26f56f9266bad121dbb91
rpantone <ross.pantone@gmail.com>
2017-09-04 20:15:23 -0400

    Add files via upload

---
+++ listingController.js
@@ -0,0 +1,26 @@
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+  function($scope, Listings) {
+    $scope.listings = Listings;
+    $scope.detailedInfo = undefined;
+    
+    /* NEW STUFF */
+    
+	$scope.finder = '';
+	
+    $scope.addListing = function() {
+		$scope.listings.push({
+			"code": $scope.code_,
+			"name": $scope.name_,
+            "address": $scope.address_
+		});
+	};
+	
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index, 1);
+	};
+    $scope.showDetails = function(index) {
+		$scope.detailedInfo = $scope.listings[index];
+	};
+	
+  }
+]);
